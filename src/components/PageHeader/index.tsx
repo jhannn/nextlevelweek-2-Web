fcd24a0ece5bd9a559282519f2fc87a74a8d9528
Jhannn <johann.guerra200@gmail.com>
2020-08-07 00:19:08 -0300

    Terceiro dia do Next Level Week #2 - Finalizando o frontend. - Conectando o front com o back.

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -6,8 +6,9 @@ import backIcon from '../../assets/images/icons/back.svg';
 
 import './styles.css';
 
-interface PageHeaderProps{
+interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
 const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
@@ -21,6 +22,7 @@ const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
             </div>
             <div className="header-content">
                 <strong>{props.title}</strong>
+                {props.description && <p>{props.description}</p>}
                 {props.children}
             </div>
         </header>
--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,33 +1,52 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+
+};
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id:teacher.id
+        });
+    };
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/6514834?s=460&v=4" alt="Foto do Proffy" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Nome do Proffy</strong>
-                    <span>Programação</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Lorem ipsum senectus proin eu aliquam morbi, tortor eget fringilla ut. curabitur vestibulum scelerisque donec dolor libero iaculis adipiscing rutrum, varius pharetra pellentesque vulputate varius libero in, viverra egestas aenean eleifend tincidunt accumsan quisque. netus conubia ornare amet adipiscing augue a nisl sed fringilla sodales varius tortor dui, donec convallis erat litora mollis enim morbi imperdiet fames massa libero primis. tristique posuere habitant morbi gravida in sed elit id, porta convallis curabitur nunc primis mattis morbi, primis torquent in pellentesque fermentum lacinia ipsum. platea etiam euismod aenean porttitor donec porttitor tellus suscipit class donec, nullam risus taciti integer etiam vehicula accumsan fermentum.
-                        <br /><br />
-                        Curabitur auctor tempor neque aliquam et quisque etiam tempus commodo, litora turpis eleifend scelerisque semper augue justo potenti habitasse iaculis, lorem dapibus sit justo aliquet curae curabitur lobortis. ipsum molestie nostra vestibulum mi facilisis dui integer dictumst gravida, diam aliquam class ullamcorper tellus habitant gravida magna, primis ligula curae mi hac cras suscipit integer. ultricies cubilia augue nisl urna habitant diam ipsum primis eu, himenaeos neque sagittis imperdiet volutpat auctor eros ipsum, fringilla vulputate rhoncus tellus cubilia lacinia odio lorem. suscipit dolor porta purus lectus convallis est dui nullam sociosqu, integer est nisl sociosqu massa adipiscing porttitor semper.
-                    </p>
+            <p>{teacher.bio}</p>
             <footer>
                 <p>
                     Preço/hora
-                        <strong>R$ 100,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
-                            Entrar em contato
-                        </button>
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     )
